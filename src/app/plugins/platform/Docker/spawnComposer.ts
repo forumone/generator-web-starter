0854dcc9f9cb2f077a374b55d53fcbdadc6fdccc
gustavderdrache <aford@forumone.com>
2019-02-27 10:38:20 -0500

    Perf: Share composer cache between runs

--- src/app/plugins/platform/Docker/spawnComposer.ts
+++ src/app/plugins/platform/Docker/spawnComposer.ts
@@ -1,5 +1,8 @@
+import envPaths from 'env-paths';
 import execa, { ExecaReturns, Options } from 'execa';
+import mkdir from 'make-dir';
 import os from 'os';
+import path from 'path';
 
 function getUserGroupOptions(): string[] {
   if (os.platform() === 'win32') {
@@ -13,6 +16,7 @@ function getUserGroupOptions(): string[] {
 export interface SpawnComposerOptions extends Options {
   runAsUser?: boolean;
   mountCwd?: boolean;
+  useCache?: boolean;
 }
 
 async function spawnComposer(
@@ -20,17 +24,32 @@ async function spawnComposer(
   {
     runAsUser = true,
     mountCwd = true,
+    useCache = true,
     ...execaOptions
   }: SpawnComposerOptions = {},
 ): Promise<ExecaReturns> {
   const userOptions = runAsUser ? getUserGroupOptions() : [];
-  const mountOptions =
+  const mountOptions: string[] = [];
+
+  if (mountCwd && execaOptions.cwd) {
     // Use 'delegated' since we presume that install-related Composer tasks are I/O
     // related, and thus can rely on the container being authoritative while the
     // process is running.
-    mountCwd && execaOptions.cwd
-      ? ['-v', execaOptions.cwd + ':/app:delegated']
-      : [];
+    mountOptions.push('-v', execaOptions.cwd + ':/app:delegated');
+  }
+
+  if (useCache) {
+    const { cache } = envPaths('generator-web-starter');
+    const composerCachePath = path.join(cache, 'composer');
+
+    try {
+      await mkdir(composerCachePath);
+      mountOptions.push('-v', composerCachePath + ':/tmp/cache:cached');
+    } catch {
+      // Ignore errors; failing to create a subdirectory in the cache isn't fatal, it's
+      // just slower.
+    }
+  }
 
   return execa(
     'docker',
